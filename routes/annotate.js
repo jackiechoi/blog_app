9ea81cd87e075f3fe095daefdf9c3a277fb957dc
Jackie <jackiechoi66@gmail.com>
2017-03-27 14:55:05 +0200

    Annotation update shipped

--- routes/annotate.js
+++ routes/annotate.js
@@ -7,65 +7,36 @@ var express = require('express'),
 
 const annotations = []
 
-// LOAD ANNOTATIONS
-router.get("/annotation/annotations", (req, res) => {
+// CREATE: STORE ANNOTATIONS
+router.post('/annotation/store', (req, res) => {
+    //Pass annotations.length to req.body.id
+    req.body.id = annotations.length;
+    annotations.push(req.body)
+    //return the id in json format
+    res.json({id: req.body.id})
+})
+
+// READ: LOAD ANNOTATIONS
+router.get('/annotation/annotations', (req, res) => {
     const reqOrigin = req.headers.referer;
     //filter through elements of annotations array and when its uri equals request origin url, we store it to pageAnnotations
     const pageAnnotations = annotations.filter( (ele) => ele.uri === reqOrigin );
     //return the saved annotation in json format
-    res.json(pageAnnotations)
+    res.json(pageAnnotations);
 })
 
-// STORE ANNOTATIONS
-router.post("/annotation/store", (req, res) => {
-    console.log("req.params: ", req.params)
-    console.log("Stored annotations: ", annotations);
-    //pass annotations.length to req.body.id
-    const ID = annotations.length;
-    req.body.id = ID
-    annotations.push(req.body)
-    //return the id in json format
-    res.json({id: ID})
+// UPDATE: UPDATE ANNOTATIONS
+router.put('/annotation/update/:id', (req, res) => {
+    //retrieve the updated data from req.body and save it to annotations array with the correct id
+    annotations[req.params.id] = req.body
+    //return the updated/saved annotation in json format
+    res.json({id: req.body.id})
 })
 
-// UPDATE ANNOTATIONS
-
-//https://blog.yipl.com.np/annotating-content-with-annotatorjs-in-webpages-2dbcaebbdf29#.25v1gyqo7
-
-/*
-var ann = new annotator.App();
-ann
-.start()
-.then(function () {
-     ann.annotations.load();
-});
-
-
-//UPDATE
-app.put('/annotation/update/:id', function(req, res){
-	$annotation = Annotation::find($id);
-    if($annotation) {
-        $data = json_decode($request->getContent(), true);
-        $annotation->ranges = $data['ranges'];
-        $annotation->quote = $data['quote'];
-        $annotation->text = $data['text'];
-        $annotation->page_id = $data['page_id'];
-
-        if($annotation->save()) {
-            return response()->json(['status' => 'success']);
-        }
-    }
-    return response()->json(['status' => 'error']);
+// DESTROY: DELETE ANNOTATIONS
+router.delete('/annotation/delete/:id', (req, res) => {
+    //res.redirect('/annotation/annotations');
+    res.send('delete path working!')
 })
-
-// DELETE
-app.delete('/annotation/delete/:id}', function(req, res){
-	if(Annotation::destroy($id)) {
-        return response()->json(['status' => 'success']);
-    }
-
-    return response()->json(['status' => 'error']);
-})
-*/
 
 module.exports = router;
